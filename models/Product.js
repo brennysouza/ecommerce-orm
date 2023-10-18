fbaab91c4008299b589ce8d298a4a926fabdaa1d
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-18 15:52:46 -0400

    added category id to product model

--- models/Product.js
+++ models/Product.js
@@ -38,7 +38,14 @@ Product.init(
         isNumeric: true 
       }
     },
-    
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id' 
+      }
+    }
+
   },
   {
     sequelize,
